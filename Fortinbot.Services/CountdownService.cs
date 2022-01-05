cb62a30de95962f716cd6705ec79ec766f28e093
Darren Swan <thegundamguru@gmail.com>
2022-01-04 18:35:50 -0600

    Adding project files

---
+++ Fortinbot.Services/CountdownService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Fortinbot.Services
+{
+	public class CountdownService : IHostedService
+	{
+		private readonly ILogger _logger;
+		public CountdownService(ILogger<CountdownService> logger, IHostApplicationLifetime appLifetime)
+		{
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			appLifetime.ApplicationStarted.Register(OnStarted);
+			appLifetime.ApplicationStopping.Register(OnStopping);
+			appLifetime.ApplicationStopped.Register(OnStopped);
+		}
+
+		private void OnStopped()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnStopping()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnStarted()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Fortinbot/Program.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Fortinbot.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortinbot
+{
+	internal class Program
+	{
+		private static DiscordSocketClient _client;
+
+		//public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			.ConfigureServices((hostcontext, services) =>
+			{
+				services.AddScoped<DiscordSocketClient>(async _ =>
+				{
+					_client = new DiscordSocketClient();
+
+					await _client.LoginAsync(TokenType.Bot,
+					Environment.GetEnvironmentVariable("DiscordToken"));
+					await _client.StartAsync();
+					return _client;
+				});
+				services.AddHostedService<CountdownService>();
+			});
+
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+			_client.Log += Log;
+			await _client.LoginAsync(TokenType.Bot,
+				Environment.GetEnvironmentVariable("DiscordToken"));
+			await _client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
